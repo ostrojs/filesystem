1fb383bc9d933ae84a43069a2fde3f2915c2755a
ostrojs <ostrojsframework@gmail.com>
2021-12-17 20:57:48 +0530

    manager improvment

--- filesystemManager.js
+++ filesystemManager.js
@@ -1,31 +1,15 @@
 require('@ostro/support/helpers')
-const { Macroable } = require('@ostro/support/macro')
-const ObjectGet = require('lodash.get')
+const Manager = require('@ostro/support/manager')
 const InvalidArgumentException = require('./invalidArgumentException')
 const FilesystemAdapter = require('./filesystemAdapter')
 const callbackHandler = require('./callbackHandler')
-const util = require('util')
-const kApp = Symbol('app')
-const kDisks = Symbol('disks')
 const kHandler = Symbol('handler')
-const kCustomCreators = Symbol('customCreators')
-class FileSystemManager extends Macroable {
-    constructor($app) {
-        super()
-        Object.defineProperties(this, {
-            [kApp]: {
-                value: $app,
-            },
-            [kDisks]: {
-                value: Object.create(null),
-                writable: true,
-            },
-            [kCustomCreators]: {
-                value: Object.create(null),
-                writable: true,
-            },
+class FileSystemManager extends Manager {
 
-        })
+    $type = 'filesystem';
+
+    constructor($app){
+        super($app)
         this[kHandler] = this.getConfig('handler')
         if (typeof this[kHandler] != 'function' || typeof this[kHandler] != 'object') {
             this[kHandler] = callbackHandler
@@ -39,45 +23,27 @@ class FileSystemManager extends Macroable {
     }
 
     drive($name = null) {
-        return this.disk($name);
-    }
-
-    driver($name = null) {
-        return this.disk($name);
+        return this.driver($name);
     }
 
     disk(name = null) {
-        name = name ? name : this.getDefaultDriver();
-        return this[kDisks][name] = this.getDriver(name);
-    }
-    cloud() {
-        var name = this.getDefaultCloudDriver();
-        return this[kDisks][name] = this.getDriver(name);
+        return this.driver(name)
     }
 
-    getDriver(name) {
-        return this[kDisks][name] || this.resolve(name);
+    cloud() {
+        var name = this.getDefaultCloudDriver();
+        return this.disk(name)
     }
 
     resolve($name) {
-        var config = this.getConfig($name);
-        if (!config) {
+        var $config = this.getConfig($name);
+        if (!$config) {
             throw new InvalidArgumentException("Disk [{" + $name + "}] was not available.");
         }
-        var driverMethod = 'create' + (config['driver']).ucfirst() + 'Driver';
-        if ((this[kCustomCreators][config['driver']])) {
-            return this.callCustomCreator(config);
-        } else if (this[driverMethod]) {
-            return this[driverMethod](config);
-        } else {
-            throw new InvalidArgumentException("Driver [{" + config['driver'] + "}] is not supported.");
-        }
+        return super.resolve($name, $config)
     }
 
-    callCustomCreator($config) {
-        return this[kCustomCreators][$config['driver']];
-    }
-
+  
     createLocalDriver($config) {
         return this.adapt(new(require('./adapter/local'))($config['root'], $config), $config);
     }
@@ -91,20 +57,17 @@ class FileSystemManager extends Macroable {
     }
 
     adapt(adapter, config) {
-        return new FilesystemAdapter(adapter, this[kHandler]);
+        return new FilesystemAdapter(adapter,this[kHandler]);
     }
 
     getConfig(name) {
-        return ObjectGet(this[kApp]['config']['filesystem']['disks'], name);
-    }
-
-    getDefaultDriver() {
-        return this[kApp]['config']['filesystem']['default'];
+        return super.getConfig(`disks.${name}`);
     }
 
     getDefaultCloudDriver() {
-        return this[kApp]['config']['filesystem']['cloud'];
+        return this.getConfig('cloud');
     }
+
     registerToRequest(FileRequest) {
         let self = this
         FileRequest.prototype.store = function(dir = '', disk = '', options = {}) {
@@ -119,17 +82,7 @@ class FileSystemManager extends Macroable {
         }
 
     }
-    extends($driver, $callback) {
-        const config = this.getConfig($driver)
-        if (!config) {
-            throw new InvalidArgumentException(`Config not found for  [{${$driver}}] driver.`);
-        }
-        this[kCustomCreators][config['driver']] = $callback.call(this, this);
-        return this;
-    }
-    __get(target, method) {
-        return this.make(target.disk(), method)
-    }
+    
 }
 
 module.exports = FileSystemManager
